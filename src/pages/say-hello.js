0a5103cfa48a0e4aa80a157571346650c81868a6
Ice <blackicezer@gmail.com>
2023-03-27 15:56:56 +0700

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/say-hello.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Greeting from "../components/greeting"
+
+const SayHello = () => {
+  return (
+    <div>
+      <Greeting name="Megan" />
+      <Greeting name="Obinna" />
+      <Greeting name="Generosa" />
+    </div>
+  )
+}
